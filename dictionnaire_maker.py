a6024bbc24082f13471bafa140b8d53793ed181d
Nassim-L <66873505+Nassim-L@users.noreply.github.com>
2020-12-14 15:29:44 +0100

    Add files via upload

---
+++ dictionnaire_maker.py
@@ -0,0 +1,64 @@
+user_list = {'sim':'shelby',}
+polling = True
+p = "done you key has been add to user list"
+while polling:
+    key = input('put yout key:  ')
+    value = input('put yout value: ')
+    if key == 'quit' or value == 'quit':
+        print("2")
+        polling = False
+        print(user_list)
+        break
+
+
+
+
+
+
+    for k in list(user_list):
+        if key == k:
+            print("3")
+            A = input('''this key name is already taken u want use another name '
+                      Yes / no :   ''')
+            if A == 'no':
+                polling = False
+            elif A == 'yes':
+                print("4")
+                key = input('put yout key:  ')
+                user_list[key] = value
+                print(p)
+        if key != k:
+            print("5")
+            user_list[key] = value
+            print(p)
+
+
+
+
+print(user_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
